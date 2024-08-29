8b0d5886382e95fa359756f566aa2a3f62098ebb
Arthur <azxu@stu.naperville203.org>
2024-08-29 14:08:01 -0500

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+
+
+import java.util.LinkedList;
+
+public class Reverse {
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static void reverse(LinkedList<String> strings) 
+    {
+
+    }
+}
